e601cd3b0300b5a2e5e1770e081cab445670605d
JoeHidemichi <h.joe.7490@gmail.com>
2019-04-29 01:25:41 +0900

    パルスノイズのシェーダーを追加。

---
+++ Assets/Materials/PulseNoise/PuluseNoiseController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuluseNoiseController : MonoBehaviour
+{
+	IEnumerator GeneratePuluseNoise()
+	{
+		for(int i = 0; i <= 180; i += 30)
+		{
+			GetComponent<Renderer>().material.SetFloat("_Amount", 0.2f * Mathf.Sin(i * Mathf.Deg2Rad));
+			yield return null;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(Mathf.Floor(Time.time % 2) == 0)
+		{
+			StartCoroutine(GeneratePuluseNoise());
+		}
+	}
+}
